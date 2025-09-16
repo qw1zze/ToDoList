7acc4f2840644897df6421af2c55632151fef534
Dmitriy Kalyakin <dmitriykalyakin@Dmitriys-MacBook-Pro.local>
2025-09-16 22:47:43 +0300

    init project

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Dmitriy Kalyakin on 16/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
